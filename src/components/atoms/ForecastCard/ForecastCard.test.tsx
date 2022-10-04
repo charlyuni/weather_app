c1a9bf8a773fd9a00dd8ca5e9c790208608bff4e
Carlos Actis <67552637+charlyuni@users.noreply.github.com>
2022-10-04 14:51:58 -0300

    Create-test-atom-ForecastCard

---
+++ src/components/atoms/ForecastCard/ForecastCard.test.tsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import ForescastCard from './ForecastCard';
+
+test('Should render component with props', () => {
+	const icon = 'SomeFakeIcon';
+    const hours = 'SomeFakeHours';
+    const temp = 10;
+
+	const component = render(<ForescastCard hours={hours} icon={icon} temp={temp} />);
+
+	expect(component.container).toBeInTheDocument();
+});
