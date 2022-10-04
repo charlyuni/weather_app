e2518722a9734d458c102d00fc860513fc43eca1
Carlos Actis <67552637+charlyuni@users.noreply.github.com>
2022-10-04 14:54:35 -0300

    Create-test-molecule-Navbar

---
+++ src/components/molecules/NavBar/NavBar.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import NavBar from './NavBar';
+
+test('Should render component with props', () => {
+    const onclick = jest.fn();
+	const onClickInput = jest.fn();
+	const onClickLocatio = jest.fn();
+	const onClickTopBotton = jest.fn();
+	const cities = Array(10).fill('SomeFakeCity');
+
+	const component = render(<NavBar  onclick={onclick} onClickInput={onClickInput} onClickLocatio={onClickLocatio} cities={cities} onClickTopBotton={onClickTopBotton}/>);
+
+	expect(component.container).toBeInTheDocument();
+});
