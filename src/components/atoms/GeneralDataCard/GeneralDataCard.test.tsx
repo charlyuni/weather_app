557e5870af39239123a9281f9896b55b2dc008b5
Carlos Actis <67552637+charlyuni@users.noreply.github.com>
2022-10-04 14:52:21 -0300

    Create-test-atom-GeneralDataCard

---
+++ src/components/atoms/GeneralDataCard/GeneralDataCard.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import GeneralDataCard from './GeneralDataCard';
+
+test('Should render component with props', () => {
+	const icons = 'SomeFakeIcon';
+    const title = 'SomeFaketitle';
+    const size = 10;
+	const value = 'SomeFakeValue';
+	const unit = 'SomeFakeUnit';
+
+	const component = render(<GeneralDataCard icons={icons} title={title} size={size} value={value} unit={unit}  />);
+
+	expect(component.container).toBeInTheDocument();
+});
