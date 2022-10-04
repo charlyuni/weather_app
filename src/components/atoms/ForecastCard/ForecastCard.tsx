cf0c4985f0e85c364ff458eb68bf9050585d61a4
Carlos Actis <67552637+charlyuni@users.noreply.github.com>
2022-10-04 14:43:45 -0300

    Create-atoms-forecastCard

---
+++ src/components/atoms/ForecastCard/ForecastCard.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react"
+
+export interface IForecastCardProps {
+    hours: string,
+    icon: string,
+    temp: number,
+}
+
+const ForecastCard: FC<IForecastCardProps> = ({hours, icon, temp}) => {
+    return (
+        <div className="flex flex-col items-center justify-around">
+            <p className="text-sm font-light">{hours}</p>
+            <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={icon} className="w-12 my-1 " />
+            <p className="font-medium">{temp} °</p>
+        </div>
+    )
+}
+export default ForecastCard
