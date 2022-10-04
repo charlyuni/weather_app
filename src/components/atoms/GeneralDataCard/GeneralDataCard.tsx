a46199182e27c9e77dfd367679dcc3e4ba83203f
Carlos Actis <67552637+charlyuni@users.noreply.github.com>
2022-10-04 14:44:36 -0300

    Create-atoms-GeneralDataCard

---
+++ src/components/atoms/GeneralDataCard/GeneralDataCard.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { WiDaySunny, WiMoonAltNew, WiThermometerExterior, WiThermometer } from "react-icons/wi";
+
+export interface ITemperatureCard {
+  icons: string;
+  title: string;
+  size: number;
+  value: string;
+  unit: string;
+}
+
+const GeneralDataCard: FC<ITemperatureCard> = ({icons, title, size, value, unit}) => {
+  return (
+    <>
+      {icons === 'WiDaySunny' ? <WiDaySunny size={size} /> : null}
+      {icons === 'WiMoonAltNew' ? <WiMoonAltNew size={size} /> : null}
+      {icons === 'WiThermometerExterior' ? <WiThermometerExterior size={size} /> : null}
+      {icons === 'WiThermometer' ? <WiThermometer size={size} /> : null}
+      <p className="font-light text-[12px]">{title}: {value} {unit}</p>
+      <p className="font-light">|</p>
+    </>
+  )
+}
+export default GeneralDataCard
